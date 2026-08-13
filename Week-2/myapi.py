515ed98afcb2e270b0b36a38a12320b4619b03cd
Smrithi P <smrithipiedy21@outlook.com>
2026-08-13 21:09:24 +0530

    Stage 1: root and health endpoints

--- Week-2/myapi.py
+++ Week-2/myapi.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+tasks = {
+    1:{ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+}
 
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
+
+
+@app.get("/get-tasks/{tasks_id}")
+def get_tasks(tasks_id: int = Path(..., description="The ID of the task to retrieve", gt=0)):
+    return tasks[tasks_id]
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
